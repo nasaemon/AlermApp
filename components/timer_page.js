84e62946522a738bce63883817c0aaa20d63df54
asan kondo <htilcs1115@gmail.com>
2018-03-13 16:16:53 +0900

    add/ Picker

--- components/timer_page.js
+++ components/timer_page.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Picker } from 'react-native'
 import { Button, Text, Header } from 'react-native-elements'
 
 export default class TimerView extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { time: null }
+  }
   render() {
+    var list = []
+    for( const i=1; i<=10; i++ ){
+      list.push( <Picker.Item label= {i+'時間'} value={i}/> )
+    }
+
     return(
-      <Header
-      leftComponent={{ icon: 'menu', color: '#fff' }}
-      centerComponent={{ text: 'Alerm APP', style: { color: '#fff' } }}
-      />
+      <View>
+        <Header
+        centerComponent={{ text: 'Alerm APP', style: { color: '#fff' } } }
+        />
+        <Picker
+          selectedValue={this.state.time}
+          onValueChange={(itemValue, itemIndex) => this.setState({time: itemValue})}>
+          { list }
+        </Picker>
+        <Text> {this.state.time} </Text>
+      </View>
     )
   }
 }
