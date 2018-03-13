e7837b05e72b4398c651cff5bc753dae644712ea
asan kondo <htilcs1115@gmail.com>
2018-03-13 17:56:53 +0900

    add /スタートボタン (native-elementを使うとButtonラベルが変わらない)

--- components/timer_page.js
+++ components/timer_page.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { StyleSheet, View, Picker } from 'react-native'
-import { Button, Text, Header } from 'react-native-elements'
+import { Button, StyleSheet, View, Picker } from 'react-native'
+import { Text, Header } from 'react-native-elements'
 
 export default class TimerView extends React.Component {
   constructor(props) {
     super(props)
     this.state = { time: 'off' }
   }
+
+  startTimer(){}
+  stopTimer(){}
   render() {
     var list = []
     for( const i=1; i<=10; i++ ){
-      list.push( <Picker.Item label= {i+'時間'} value={i}/> )
+      list.push( <Picker.Item label= {i+'時間'} value={i} key={i*i}/> )
     }
 
     return(
@@ -24,7 +27,9 @@ export default class TimerView extends React.Component {
           <Picker.Item label='OFF' value='off' />
           { list }
         </Picker>
-        <Text> {this.state.time} </Text>
+
+        <Button title={'Start Timer!!'} onPress={ () => this.startTimer() } />
+        <Button title={'Stop Timer!!'} onPress={ () => this.stopTimer() } />
       </View>
     )
   }
